8be41efe222b7f5f3bbf027d4151bbda876ff574
Bogus <jablonski.boguslaw@gmail.com>
2020-12-08 23:13:55 +0100

    init commit

---
+++ src/main/java/pl/bogus/demo/HellotrelloApplication.java
@@ -0,0 +1,13 @@
+package pl.bogus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellotrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellotrelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bogus/demo/HellotrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bogus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellotrelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
